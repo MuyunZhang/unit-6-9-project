af5f759e0113a6e54fdfcaff01de82c3b01320eb
BT_1N3_21 <muyunz@nycstudents.net>
2024-03-15 09:10:51 -0400

    Initial

---
+++ src/Player.java
@@ -0,0 +1,18 @@
+public class Player extends Space {
+    private int score;
+    private String name;
+
+    public Player(String name) {
+        super(name.substring(0, 1)); // symbol is first initial
+        score = 0;
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void addPoints(int amt) {
+        score += amt;
+    }
+}
